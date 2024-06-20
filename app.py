6b88240d77f9852e9c41bd9fd4a49244c7c3ece5
Nikoloz Ekhvaia <ekhvaianika@gmail.com>
2024-06-20 19:14:43 +0400

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash, get_flashed_messages
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from api import Film
 
 app = Flask(__name__)
 app.secret_key = "bacho123"
 
-@app.route('/')
-@app.route("/home")
+
+@app.route('/', methods=["POST", "GET"])
+@app.route("/home", methods=["POST", "GET"])
 def home():
+    if request.method == "POST":
+        print("Form Data:", request.form)
+        if "user_input" in request.form:
+            usr_inpt = request.form["user_input"]
+            movie = Film(usr_inpt)
+
+            movie_data = movie.response
+            session['movie_data'] = movie_data
+
+            return redirect(url_for('result'))
+        else:
+            flash('No input received. Please enter a film name.')
+            return redirect(url_for('home'))
     return render_template("indexx.html")
 
 
@@ -19,6 +34,18 @@ def login():
         session["user_email"] = user
         return redirect('/home')
     return render_template("loginn.html")
+
+
+@app.route('/logout')
+def logout():
+    session.pop("user_email", None)
+    session.pop("movie_data", None)
+    return redirect('/login')
+
+
+@app.route('/result')
+def result():
+    return render_template("result.html")
 
 
 if __name__ == "__main__":
